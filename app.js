4eee211432caed7ced0a0400919ad02bf0482ebf
William Chalk <3halk1@gmail.com>
2022-09-23 21:48:47 -0400

    added html generator function

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require("fs");
 
-const printProfileData = (profileDataArr) => {
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const generatePage = require("./src/page-template.js");
 
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [userName, github] = profileDataArgs;
+// console.log(profileDataArgs);
+const pageHTML = generatePage(userName, github);
+// const printProfileData = (profileDataArr) => {
+//   profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+fs.writeFile("./index.html", pageHTML, (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Portfolio complete! Checkout index.html to see the output!");
+});
