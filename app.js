49e654904b560e225377e5e3272a79c9b2b4b00b
William Chalk <3halk1@gmail.com>
2022-09-24 18:32:19 -0400

    added site generation features

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const { type } = require("os");
 
-const fs = require("fs");
+const { writeFile, copyFile } = require("./utils/generate-site");
 
 const generatePage = require("./src/page-template.js");
 
@@ -142,9 +142,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw new Error(err);
-      console.log("Portfolio complete! Checkout index.html to see the output!");
-    });
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
   });
