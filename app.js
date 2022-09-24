b9f041695426014a3a0751ca49ffbe6c39fd31b0
William Chalk <3halk1@gmail.com>
2022-09-24 17:11:43 -0400

    added project html generation

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const inquirer = require("inquirer");
 const { type } = require("os");
 
-// const fs = require("fs");
+const fs = require("fs");
 
-// const generatePage = require("./src/page-template.js");
-
-// const pageHTML = generatePage(userName, github);
+const generatePage = require("./src/page-template.js");
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -144,10 +142,9 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+      console.log("Portfolio complete! Checkout index.html to see the output!");
+    });
   });
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw new Error(err);
-
-//   console.log("Portfolio complete! Checkout index.html to see the output!");
-// });
